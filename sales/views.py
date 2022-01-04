6df6b580f3fa1826d25561c60b24da56c6052681
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-04 23:08:28 +0900

    Ch11 - template html에 정보(static, db정보) 전달과 display

--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from .models import 아이디
 
 def homepage(request):
-    return render(request, "anyfile2.html")
+    guests = 아이디.objects.all()
+    context = { 
+        "menu_name" : "짜장",
+        "price" : "5000원",
+        "guests" : guests
+    }
+    return render(request, "anyfile2.html", context)
