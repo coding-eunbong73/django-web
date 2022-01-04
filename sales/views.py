a438d5e3635a0a968f293124ade0859342eb2aef
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-04 16:30:20 +0900

    Ch10 - render이용하여 html문서 출력

--- sales/views.py
+++ sales/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def homepage(request):
-    return HttpResponse("응 나야")
+    return render(request, "folder/anyfile1.html")
