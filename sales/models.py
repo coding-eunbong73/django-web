a2255e62873ce40d37d4cd3f369ab45e2a764d75
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-04 10:33:14 +0900

    add ForeignKey and class person

--- sales/models.py
+++ sales/models.py
@@ -5,3 +5,8 @@ class Sale(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE)
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)	    
