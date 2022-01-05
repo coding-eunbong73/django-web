9a574294314e1b2270721d0e60381200af2ee6e4
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-06 01:19:19 +0900

    Ch20 - url namespace 활용

--- sales/urls.py
+++ sales/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import homepage
 from .views import salesList, saleDetail, saleForm, saleUpdate, saleDelete
 
-app_name = "homepage"
+app_name = "sales"
 
 urlpatterns = [
     #path ('', homepage),
-    path ('', salesList),
-    path ('make/', saleForm),
-    path ('<int:pk>/', saleDetail),
-    path ('<int:pk>/update', saleUpdate),
-    path ('<int:pk>/delete', saleDelete),
+    path ('', salesList, name='list'),
+    path ('make/', saleForm, name='create'),
+    path ('<int:pk>/', saleDetail, name='detail'),
+    path ('<int:pk>/update', saleUpdate, name='update'),
+    path ('<int:pk>/delete', saleDelete, name='delete'),
 ]
