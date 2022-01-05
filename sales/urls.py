f7cb58dc08b2a87c617b767b125b42cbb93ce8a8
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-05 11:04:44 +0900

    Ch13 - Url 파라미터 처리

--- sales/urls.py
+++ sales/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import homepage
+from .views import salesList, saleDetail
 
 app_name = "homepage"
 
 urlpatterns = [
-    path ('', homepage)
+    path ('', homepage),
+    path ('sales', salesList),
+    path ('sales/<pk>', saleDetail)
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-from .models import 아이디
+from .models import 아이디, Sale
 
 def homepage(request):
     guests = 아이디.objects.all()
@@ -10,3 +10,15 @@ def homepage(request):
         "guests" : guests
     }
     return render(request, "anyfile2.html", context)
+
+def salesList(request):
+    saleGuests = Sale.objects.all()
+    context = { 
+        "saleGuests" : saleGuests
+    }
+    return render(request, "salesList.html", context)
+
+def saleDetail(request, pk):
+    print (pk)
+    responseStr = "여기상세 정보입니다." + pk
+    return HttpResponse(responseStr)
