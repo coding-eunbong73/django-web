ef66434e189927d505bc242f914b13859994a561
EunBong Lee <eunbong73.lee@gmail.com>
2022-01-06 18:16:21 +0900

    Ch24 - static 설정 및 적용

--- djangocrm/urls.py
+++ djangocrm/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from sales.views import homepage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
     path('sales/', include('sales.urls', namespace="sales")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
